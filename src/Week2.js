ecf32c32426158016c7e7534216ea886e24480b6
FiyinfoluwaE <fiyinfoluwasowunmi@gmail.com>
2023-09-14 20:39:40 +0100

    Made icon weather icon live

--- src/Week2.js
+++ src/Week2.js
@@ -17,9 +17,12 @@ function third(event) {
   let cityInput = document.querySelector("#searchInput");
   let city = `${cityInput.value}`;
   let apiKey = `2857bd67083147845b42e714a671529d`;
+  let apiKey1 = `5b45b512306f330fb43aob2122bt1dc0`;
   let unit = `metric`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  let apiUrl1 = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey1}&units=${unit}`;
   axios.get(apiUrl).then(third1);
+  axios.get(apiUrl1).then(third4);
 }
 let formInput = document.querySelector("#searchBar");
 formInput.addEventListener("submit", third);
@@ -45,21 +48,32 @@ function third1(response) {
 
 function third3(position) {
   let apiKey = `2857bd67083147845b42e714a671529d`;
+  let apiKey1 = `5b45b512306f330fb43aob2122bt1dc0`;
   let unit = `metric`;
   let lat = `${position.coords.latitude}`;
   let lon = `${position.coords.longitude}`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+  let apiUrl1 = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey1}&units=${unit}`;
   axios.get(apiUrl).then(third1);
+  axios.get(apiUrl1).then(third4);
 }
 function third2() {
   navigator.geolocation.getCurrentPosition(third3);
 }
 let currentCityButton = document.querySelector("#currentCity");
 currentCityButton.addEventListener("click", third2);
+function third4(response) {
+  console.log(response.data.condition.icon_url);
+  let icon1 = `${response.data.condition.icon}`;
+  let icon = document.querySelector("#icon");
+  icon.setAttribute("src", `${response.data.condition.icon_url}`);
+  let description = document.querySelector("#weatherDescription");
+  description.innerHTML = `${response.data.condition.description}`;
+}
 
 function fourth() {
   let current = new Date();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thursday", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
   let day = days[current.getDay()];
   let date = current.getDate();
   let time = current.toLocaleString([], {
