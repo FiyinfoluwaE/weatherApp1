d588844f554a433ab047cea3730bdf4bec1065a1
FiyinfoluwaE <fiyinfoluwasowunmi@gmail.com>
2023-09-15 21:21:10 +0100

    deleted and renamed functions

--- src/Week2.js
+++ src/Week2.js
@@ -1,37 +1,24 @@
-function first(event) {
-  event.preventDefault();
-}
-let cel = document.querySelector("#celsius");
-cel.addEventListener("click", first);
-
-function second(event) {
-  event.preventDefault();
-  let fah = document.querySelector("#temperature");
-  fah.innerHTML = 27 * 1.8 + 32;
-}
-let fah = document.querySelector("#fahrenheit");
-fah.addEventListener("click", second);
-
-function third(city) {
+function citySearch(city) {
   let apiKey = `2857bd67083147845b42e714a671529d`;
   let apiKey1 = `5b45b512306f330fb43aob2122bt1dc0`;
   let unit = `metric`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
   let apiUrl1 = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey1}&units=${unit}`;
-  axios.get(apiUrl).then(third1);
-  axios.get(apiUrl1).then(third4);
+  axios.get(apiUrl).then(weatherInfo);
+  axios.get(apiUrl1).then(weatherIcon);
 }
-function third5(event) {
+citySearch("Ikorodu");
+
+function citySearch2(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#searchInput");
   let city = `${cityInput.value}`;
-  third(city);
+  citySearch(city);
 }
-third("Ikorodu");
 let formInput = document.querySelector("#searchBar");
-formInput.addEventListener("submit", third5);
+formInput.addEventListener("submit", citySearch2);
 
-function third1(response) {
+function weatherInfo(response) {
   let cityName = `${response.data.name}`;
   let country = `${response.data.sys.country}`;
   let temperature = Math.round(response.data.main.temp);
@@ -50,7 +37,7 @@ function third1(response) {
   humi.innerHTML = `Humidity: ${humidity}`;
 }
 
-function third3(position) {
+function currentCoordinate(position) {
   let apiKey = `2857bd67083147845b42e714a671529d`;
   let apiKey1 = `5b45b512306f330fb43aob2122bt1dc0`;
   let unit = `metric`;
@@ -58,15 +45,16 @@ function third3(position) {
   let lon = `${position.coords.longitude}`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
   let apiUrl1 = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey1}&units=${unit}`;
-  axios.get(apiUrl).then(third1);
-  axios.get(apiUrl1).then(third4);
+  axios.get(apiUrl).then(weatherInfo);
+  axios.get(apiUrl1).then(weatherIcon);
 }
-function third2() {
-  navigator.geolocation.getCurrentPosition(third3);
+function coordinate() {
+  navigator.geolocation.getCurrentPosition(currentCoordinate);
 }
 let currentCityButton = document.querySelector("#currentCity");
-currentCityButton.addEventListener("click", third2);
-function third4(response) {
+currentCityButton.addEventListener("click", coordinate);
+
+function weatherIcon(response) {
   console.log(response.data.condition.icon_url);
   let icon1 = `${response.data.condition.icon}`;
   let icon = document.querySelector("#icon");
